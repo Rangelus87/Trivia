e536a03c9115a1f0ad09e6c3d281e5b40048f4b9
Rangelus87 <rangelus87@gmail.com>
2023-04-10 21:28:52 -0300

    Delete motociclista directory

--- motociclista/main.py
+++
@@ -1,7 +0,0 @@
-"""motociclista/
-1-> Construye una lista de horarios que tenga intervalos de tiempo que comience a las 8:00 H y termine a las 20:00 H, con intervalos de media hora cada uno
-    * se tiene disponibilidad de 8 motociclistas cada 30 minutos
-2-> Crear un contador que comience en 8 y vaya disminuyendo a medida que el usuario va tomando recursos(motociclistas) se debe de marcar cada caja en verde
-    *las cajas son la representación de cada franja horaria
-    * si el mismo usuario vuelve a pinchar en la caja libera el motociclista
-"""
